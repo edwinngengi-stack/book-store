3843797677200c27266021ee74af9c0c1d1ca154
edwinngengi-stack <edwinngengi@gmail.com>
2026-09-10 16:10:46 +0300

    Fix database to sqlite and configure api environment variables

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL", "postgresql://booked_user:booked_pass@localhost:5432/booked_db"
+        "DATABASE_URL", "sqlite:///db.sqlite3"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "dev-jwt-secret")
